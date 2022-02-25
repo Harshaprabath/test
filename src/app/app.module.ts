5c38754747d0aa799176ff651868ced6b2895dfc
Harshaprabath <prabhathhemasiri@gmail.com>
2022-02-25 16:22:54 +0530

    install anguler app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,59 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarListComponent } from './car-list/car-list.component';
+import { CarDetailsComponent } from './car-details/car-details.component';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+const routes: Routes   = [
+
+  {
+
+    path: '',
+
+    redirectTo: 'users',
+
+    pathMatch: 'full',
+
+  },
+
+  {
+
+    path: 'car-list',
+
+    component: CarListComponent,
+
+  },
+
+  {
+
+    path: 'car-details',
+
+    component: CarDetailsComponent,
+
+  },
+  {
+
+    path: '**',
+
+    component: PageNotFoundComponent,
+
+  },
+
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarListComponent,
+    CarDetailsComponent,
   ],
+  exports: [RouterModule],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    [RouterModule.forChild(routes)],
+
   ],
   providers: [],
   bootstrap: [AppComponent]
